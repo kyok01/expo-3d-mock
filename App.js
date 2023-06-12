11deed7dc760605e1fb640086c8c478f3f26bc6f
kyokoi_japan <kazutakayokoils@gmail.com>
2023-06-12 10:18:50 +0900

    add: obj file

--- App.js
+++ App.js
@@ -1,65 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View } from 'react-native';
 import { GLView } from 'expo-gl';
+import { Renderer } from 'expo-three';
+import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
 export default function App() {
+  const glRef = useRef(null);
+
+  const onContextCreate = async (gl) => {
+    // Create a renderer
+    const renderer = new Renderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    // Create a scene
+    const scene = new THREE.Scene();
+
+    // Create a camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 5;
+
+    // Create a light
+    const light = new THREE.DirectionalLight(0xffffff, 0.5);
+    light.position.set(0, 0, 5);
+    scene.add(light);
+
+    // Load the model
+    const loader = new OBJLoader();
+    loader.load(
+      'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/models/obj/walt/WaltHead.obj',
+      (object) => {
+        // Add the model to the scene
+        scene.add(object);
+        // Start the animation loop
+        animate(renderer, scene, camera);
+      }
+    );
+  };
+
+  const animate = (renderer, scene, camera) => {
+    requestAnimationFrame(() => animate(renderer, scene, camera));
+    renderer.render(scene, camera);
+    glRef.current.endFrameEXP();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <GLView style={{ width: 300, height: 300 }} onContextCreate={onContextCreate} />
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <GLView
+        ref={glRef}
+        style={{ flex: 1 }}
+        onContextCreate={onContextCreate}
+      />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-function onContextCreate(gl) {
-  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-  gl.clearColor(0, 1, 1, 1);
-
-  // Create vertex shader (shape & position)
-  const vert = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(
-    vert,
-    `
-    void main(void) {
-      gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-      gl_PointSize = 150.0;
-    }
-  `
-  );
-  gl.compileShader(vert);
-
-  // Create fragment shader (color)
-  const frag = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(
-    frag,
-    `
-    void main(void) {
-      gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-    }
-  `
-  );
-  gl.compileShader(frag);
-
-  // Link together into a program
-  const program = gl.createProgram();
-  gl.attachShader(program, vert);
-  gl.attachShader(program, frag);
-  gl.linkProgram(program);
-  gl.useProgram(program);
-
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, 1);
-
-  gl.flush();
-  gl.endFrameEXP();
 }
