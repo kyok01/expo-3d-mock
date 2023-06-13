8fa533746fc341901a5cd0053f60898308cac09c
kyokoi_japan <kazutakayokoils@gmail.com>
2023-06-13 10:29:18 +0900

    got error, but temporary save

--- App.js
+++ App.js
@@ -1,65 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { GLView } from 'expo-gl';
+import React, { useRef } from 'react'
+import { GLView } from 'expo-gl'
+import { Renderer } from 'expo-three'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <GLView style={{ width: 300, height: 300 }} onContextCreate={onContextCreate} />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      <GLView
+        style={{ flex: 1 }}
+        onContextCreate={async (gl) => {
+          // GL Parameter disruption
+          const {
+            drawingBufferWidth: width,
+            drawingBufferHeight: height,
+          } = gl
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          // Scene
+          const scene = new THREE.Scene()
 
-function onContextCreate(gl) {
-  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-  gl.clearColor(0, 1, 1, 1);
+          // Camera
+          const camera = new THREE.PerspectiveCamera(
+            75,
+            width / height,
+            1,
+            1000
+          )
+          camera.position.z = 2
+          camera.position.x = 2
+          camera.position.y = 2
+          camera.lookAt(cube.position)
+          setCamera(camera)
+          scene.add(camera)
 
-  // Create vertex shader (shape & position)
-  const vert = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(
-    vert,
-    `
-    void main(void) {
-      gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-      gl_PointSize = 150.0;
-    }
-  `
-  );
-  gl.compileShader(vert);
+          // Renderer
+          const renderer = new Renderer({ gl })
+          renderer.setSize(width, height)
+          renderer.setClearColor('#fff')
 
-  // Create fragment shader (color)
-  const frag = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(
-    frag,
-    `
-    void main(void) {
-      gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-    }
-  `
-  );
-  gl.compileShader(frag);
+          // GLTF loader
+          const gltfLoader = new GLTFLoader()
 
-  // Link together into a program
-  const program = gl.createProgram();
-  gl.attachShader(program, vert);
-  gl.attachShader(program, frag);
-  gl.linkProgram(program);
-  gl.useProgram(program);
+          // Load model from URL or local file
+          gltfLoader.load(
+            // resource URL or file path
+            '/assets/adamHead/adamHead.gltf',
+            // called when the resource is loaded
+            function (gltf) {
+              scene.add(gltf.scene)
+            },
+            // called while loading is progressing
+            function (xhr) {
+              console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+            },
+            // called when loading has errors
+            function (error) {
+              console.log('An error happened', error)
+            }
+          )
 
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, 1);
-
-  gl.flush();
-  gl.endFrameEXP();
+          // Render function
+          const render = () => {
+            requestAnimationFrame(render)
+            renderer.render(scene, camera)
+            gl.endFrameEXP()
+          }
+          render()
+        }}
+      />
+  )
 }
